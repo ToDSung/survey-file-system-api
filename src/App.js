5be03d60e315e048d2ae303f216886418887853a
Tod Sung <tod_sung@htc.com>
2022-03-28 11:31:00 +0800

    feat: add file system api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const getFile = async () => {
+  // open file picker
+  const [fileHandle] = await window.showOpenFilePicker();
+
+  // get file contents
+  const fileData = await fileHandle.getFile();
+  console.log(fileData)
+}
+
+const directoryStructure = {}
+
+const getDirectory = async () => {
+  const directoryHandle = await window.showDirectoryPicker();
+  console.log(directoryHandle)
+  for await (const fileHandle of getFilesRecursively(directoryHandle, directoryStructure)) {
+    // console.log(fileHandle);
+  }
+
+  console.log(directoryStructure)
+}
+
+async function* getFilesRecursively (entry, currentDirectory) {
+  if (entry.kind === 'file') {
+    const file = await entry.getFile();
+    if (file !== null) {
+      // file.relativePath = getRelativePath(entry);
+      currentDirectory[entry.name] = [file]
+      // yield file;
+    }
+  } else if (entry.kind === 'directory') {
+    currentDirectory[entry.name] = {}
+    for await (const handle of entry.values()) {
+      yield* getFilesRecursively(handle, currentDirectory[entry.name]);
+    }
+  }
+}
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getFile}>
+        getFile
+      </button>
+      <button onClick={getDirectory}>
+        getDirectory
+      </button>
     </div>
   );
 }
